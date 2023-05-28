a105e2e39a04ff1dce8ceb18e6060f27a0993a5e
Abhishek Anand <anandabhishek3607@gmail.com>
2023-05-28 13:08:35 +0530

    28th May commit

--- app.js
+++ app.js
@@ -6,3 +6,26 @@ const isRightTriangle = (a, b, c) => {
 };
 
 isRightTriangle(26, 11, 12)
+
+
+const delayedColorChange = (newColor, delay, doNext) => {
+    setTimeout(() => {
+        document.body.style.backgroundColor = newColor;
+        doNext && doNext();
+    }, delay)
+}
+
+delayedColorChange('orange', 3000, () => {
+    delayedColorChange('orange', 1000, () => {
+        delayedColorChange('blue', 2000, () => {
+            delayedColorChange('blue', 3000, () => {
+                delayedColorChange('brown', 1000, () => {
+
+                })
+            });
+        });
+    });
+});
+
+
+// delayedColorChange('red', 5000);
