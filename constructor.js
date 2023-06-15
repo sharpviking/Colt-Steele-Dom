6f650d7c6f174ac03937c0e9c5895ae8779aaf1b
Abhishek Anand <anandabhishek3607@gmail.com>
2023-06-15 13:38:19 +0530

    working with constructor in js

---
+++ constructor.js
@@ -0,0 +1,10 @@
+function Color(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+}
+
+Color.prototype.rgb = function () {
+    const { r, g, b } = this;
+    return `rgb(${r}, ${g}, ${b})`;
+};
