089c6991fbaeae7d56ed989776089845284689f8
Abhishek Anand <anandabhishek3607@gmail.com>
2023-06-19 23:58:27 +0530

    another commit

---
+++ classes.js
@@ -0,0 +1,10 @@
+class Color {
+    constructor(r, g, b, name) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.name = name;
+    }
+}
+
+const c1 = new Color(253, 64, 87, 'tomato');
